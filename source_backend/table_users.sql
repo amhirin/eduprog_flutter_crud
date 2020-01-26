5a8aa6f861f378e5812df4c10b926c66715cefb6
A.M Hirin <30552503+amhirin@users.noreply.github.com>
2020-01-26 20:39:50 +0700

    ada yang ketinggalan untuk login, session, dan timeout

--- source_backend/table_users.sql
+++ source_backend/table_users.sql
@@ -7,3 +7,18 @@ CREATE TABLE `users` (
   `user_session` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+INSERT INTO `users`
+(
+`user_name`,
+`user_password`,
+`user_full_name`,
+`last_request`,
+`user_session`)
+VALUES
+(
+'admin',
+'202cb962ac59075b964b07152d234b70',
+'Si Admin',
+null,
+'');
